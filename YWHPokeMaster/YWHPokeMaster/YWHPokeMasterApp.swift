9a691e1c7bf98ff10aa20c79863d201d82562451
ywh <862638933@qq.com>
2025-04-21 15:38:31 +0800

    cicd:  新建PokeMaster项目

---
+++ YWHPokeMaster/YWHPokeMaster/YWHPokeMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  YWHPokeMasterApp.swift
+//  YWHPokeMaster
+//
+//  Created by Wells on 2025/4/21.
+//
+
+import SwiftUI
+
+@main
+struct YWHPokeMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YWHPokeMaster/YWHPokeMasterTests/YWHPokeMasterTests.swift
@@ -0,0 +1,17 @@
+//
+//  YWHPokeMasterTests.swift
+//  YWHPokeMasterTests
+//
+//  Created by Wells on 2025/4/21.
+//
+
+import Testing
+@testable import YWHPokeMaster
+
+struct YWHPokeMasterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
