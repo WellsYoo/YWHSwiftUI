a2aaf9cfcdf21d30305a4271df692218ea5faba4
ywh <862638933@qq.com>
2025-04-22 18:28:01 +0800

    cicd:  swiftUI 列表cell

---
+++ YWHPokeMaster/YWHPokeMaster/View/PokemonList.swift
@@ -0,0 +1,19 @@
+//
+//  PokemonList.swift
+//  YWHPokeMaster
+//
+//  Created by Wells on 2025/4/22.
+//
+import SwiftUI
+
+struct PokemonList: View {
+    var body: some View {
+
+    }
+
+}
+
+
+#Preview {
+    PokemonList()
+}
