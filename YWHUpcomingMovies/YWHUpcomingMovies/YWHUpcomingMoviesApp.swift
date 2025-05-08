e3978ff25a75464361957a18b52914d8d9df83af
ywh <862638933@qq.com>
2025-05-08 17:20:51 +0800

    cicd: 图片资源添加

---
+++ YWHUpcomingMovies/YWHUpcomingMovies/YWHUpcomingMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  YWHUpcomingMoviesApp.swift
+//  YWHUpcomingMovies
+//
+//  Created by Wells on 2025/5/8.
+//
+
+import SwiftUI
+
+@main
+struct YWHUpcomingMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YWHUpcomingMovies/YWHUpcomingMoviesTests/YWHUpcomingMoviesTests.swift
@@ -0,0 +1,17 @@
+//
+//  YWHUpcomingMoviesTests.swift
+//  YWHUpcomingMoviesTests
+//
+//  Created by Wells on 2025/5/8.
+//
+
+import Testing
+@testable import YWHUpcomingMovies
+
+struct YWHUpcomingMoviesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
